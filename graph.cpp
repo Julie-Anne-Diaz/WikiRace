e0aec300d70260a58161966d6dbb92f8ee64a636
Julian Diaz <jdiaz71126@gmail.com>
2025-11-03 13:35:52 -0500

    BFS

--- graph.cpp
+++ graph.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <map>
 #include <iostream>
+#include <stack>
+#include <queue>
 
 WikiGraph::WikiGraph(){}
 WikiGraph::WikiGraph(std::string filepath){makeGraph(filepath);}
@@ -21,8 +23,8 @@ bool WikiGraph::makeGraph(std::string filepath){
     std::string line;
     std::string cur;
     while (std::getline(file, line)) {
-        if (line.find("##-")==0){
-            cur=line.substr(3);
+        if (line==""){
+            std::getline(file, cur)
         }
         else{
             adj[cur].push_back(line);
@@ -32,7 +34,25 @@ bool WikiGraph::makeGraph(std::string filepath){
     return true;
 }
 int WikiGraph::BFS(std::string start, std::string end){
-    return 0;
+    int count = 0;
+    std::stack<std::string> visited;
+    std::queue q;
+    q.push(start);
+    while (!q.empty()){
+        std::string cur = q.front();
+        q.pop();
+        count++;
+        if (cur==end){
+            break;
+        }
+        for (std::string s : adj[cur]){
+            if (!visited.count(s)){
+                visited.insert(s);
+                q.push(s);
+            }
+        }
+    }
+    return count;
 }
 int WikiGraph::DFS(std::string start, std::string end){
     return 0;
